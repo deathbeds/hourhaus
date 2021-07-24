74dd1c1c4ac140de6350ec4ea9f20ec8c17df56f
Tony Fast <tony.fast@gmail.com>
2021-07-23 20:57:18 -0400

    Update noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,14 @@
 from nox import session
 
+@nox.session(venv_backend="conda", reuse_venv=True)
 def sphinx(session):
   session.install("jupyter-book")
+
+@nox.session(venv_backend="conda", reuse_venv=True)
+def mkdocs(session):
+  session.install("mkdocs")
+  
+@nox.session(venv_backend="conda", reuse_venv=True)
+def nikola(session):
+  session.install("nikola", "ruamel.yaml", "notebook")
+
