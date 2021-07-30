b4e135e7fb8204fdf323114eced60fb514844397
tonyfast <tony.fast@gmail.com>
2021-07-30 10:34:50 -0400

    install pidgy kernel

--- hourhaus.py
+++ hourhaus.py
@@ -1,11 +1,25 @@
 DOIT_CONFIG = dict(verbosity=2)
 
+
 def task_install_nox():
     return dict(actions=["nox --install-only"])
+
+
+def task_kernelspec():
+    def install():
+        from jupyter_client.kernelspec import KernelSpecManager
+        import pidgy, pathlib
+
+        KernelSpecManager().install_kernel_spec(
+            str(pathlib.Path(pidgy.__file__).parent / "kernelspec"), "pidgy", user=True
+        )
+
+    return dict(actions=[install])
+
 
 if __name__ == "__main__":
     from doit.cmd_base import ModuleTaskLoader
     from doit.doit_cmd import DoitMain
     import sys
-    
+
     sys.exit(DoitMain(ModuleTaskLoader(globals())).run(sys.argv[1:]))
