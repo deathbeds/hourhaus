42f5669be320286a320bc5952b66c66a2ea3f9a6
Tony Fast <tony.fast@gmail.com>
2021-08-10 21:06:04 -0600

    don't commit - modifications for an a writers workshop on personal style (#2)
    add graphviz, pidgy appdir

--- noxfile.py
+++ noxfile.py
@@ -1,14 +1,16 @@
 import nox
 
+
 @nox.session(venv_backend="conda", reuse_venv=True)
 def sphinx(session):
-  session.install("jupyter-book")
+    session.install("jupyter-book")
+
 
 @nox.session(venv_backend="conda", reuse_venv=True)
 def mkdocs(session):
-  session.install("mkdocs")
-  
+    session.install("mkdocs")
+
+
 @nox.session(venv_backend="conda", reuse_venv=True)
 def nikola(session):
-  session.install("nikola", "ruamel.yaml", "notebook")
-
+    session.install("nikola", "ruamel.yaml", "notebook")
